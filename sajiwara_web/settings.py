627d90e816f8bc29b020c7f91a80f2d392f801f2
Fumuri <muhfakhri30@gmail.com>
2024-10-18 14:02:12 +0700

    landingpage berhasil, athentication belum terintegrasi

--- sajiwara_web/settings.py
+++ sajiwara_web/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'sajiwara_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
