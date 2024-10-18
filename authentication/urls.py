84b5c9b8395885111bc7618933783379251c5092
theretari <theretarianingsih@gmail.com>
2024-10-18 14:14:25 +0700

    help

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,7 +5,4 @@ app_name = 'authentication'
 urlpatterns = [
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
-    path('register/', register, name='register'),
-    path('is-anonymous/', check_is_anonymous, name='check_is_anonymous'),
-    path('get-user-data/', get_user_data, name='get_user_data'),
 ]
