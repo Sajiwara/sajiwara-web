5ccf3a515850f4748e9e4eba8e2822a8bd39fcff
Fumuri <muhfakhri30@gmail.com>
2024-10-23 22:39:54 +0700

    berhasil menambah edit tapi belum diedit

--- tipemakanan/forms.py
+++ tipemakanan/forms.py
@@ -3,12 +3,28 @@ from .models import Makanan
 
 class PreferensiForm(forms.Form):
     preferensi = forms.ChoiceField(
-        choices=[],
+        choices=[('', 'Pilih negara')],
         label="Pilih Preferensi Negara",
-        widget=forms.Select(attrs={'class': 'form-control'})
+        required=True
+        # widget=forms.Select(attrs={'class': 'form-control'})
     )
 
     def __init__(self, *args, **kwargs):
         super(PreferensiForm, self).__init__(*args, **kwargs)
         # Mengambil preferensi unik dari model Makanan untuk form
-        self.fields['preferensi'].choices = [(m, m) for m in Makanan.objects.values_list('preferensi', flat=True).distinct()]
+        self.fields['preferensi'].choices += [(m, m) for m in Makanan.objects.values_list('preferensi', flat=True).distinct()]
+
+class MakananUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Makanan
+        fields = ['preferensi', 'menu', 'restoran']  # Fields yang bisa diubah oleh user
+        labels = {
+            'preferensi': 'Preferensi Negara',
+            'menu': 'Variasi Makanan',
+            'restoran': 'Nama Restoran',
+        }
+        widgets = {
+            'preferensi': forms.TextInput(attrs={'class': 'form-control'}),
+            'menu': forms.TextInput(attrs={'class': 'form-control'}),
+            'restoran': forms.TextInput(attrs={'class': 'form-control'}),
+        }
