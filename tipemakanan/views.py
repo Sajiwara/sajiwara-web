3f923cc05640871455abf2ad77478752a8174385
Fumuri <muhfakhri30@gmail.com>
2024-12-20 16:11:57 +0700

    penyesuaian untuk edit_makanan di flutter

--- tipemakanan/views.py
+++ tipemakanan/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.core import serializers
+import json
 # Create your views here.
 
 @csrf_exempt
@@ -40,6 +41,41 @@ def edit_makanan(request, pk):
         form_html = render_to_string('edit_makanan.html', {'form': form, 'makanan': makanan}, request=request)
         return JsonResponse({'form_html': form_html})
 
+@csrf_exempt
+def edit_makanan_flutter(request, pk):
+    try:
+        makanan = get_object_or_404(Makanan, pk=pk)
+
+        if request.method == 'GET':  # Untuk mendapatkan detail makanan
+            data = {
+                "id": makanan.pk,
+                "fields": {
+                    "restoran": makanan.restoran,
+                    "preferensi": makanan.preferensi,
+                    "menu": makanan.menu,
+                }
+            }
+            return JsonResponse(data, safe=False)
+
+        elif request.method == 'POST':  # Untuk mengupdate makanan
+            body = json.loads(request.body)  # Parsing data dari request body
+            restoran = body.get('restoran', makanan.restoran)
+            preferensi = body.get('preferensi', makanan.preferensi)
+            menu = body.get('menu', makanan.menu)
+
+            # Update fields
+            makanan.restoran = restoran
+            makanan.preferensi = preferensi
+            makanan.menu = menu
+            makanan.save()
+
+            return JsonResponse({'success': True, 'message': 'Makanan updated successfully!'})
+
+        else:
+            return JsonResponse({'success': False, 'message': 'Invalid HTTP method'}, status=405)
+    except Exception as e:
+        return JsonResponse({'success': False, 'message': str(e)}, status=400)
+    
 def show_xml_by_id(request, id):
     data = Makanan.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
