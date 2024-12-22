e808a6db30854b5728ef75eed14ad13319acca7a
theretari <theretarianingsih@gmail.com>
2024-12-22 11:28:46 +0700

    fixing bugs

--- wishlistresto/urls.py
+++ wishlistresto/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from wishlistresto.views import show_wishlistresto, add_to_wishlist, visited_resto, delete_wishlist,not_visited_resto, show_json, add_to_wishlist_flutter, visited_resto_flutter, delete_wishlist_flutter
+from wishlistresto.views import show_wishlistresto, add_to_wishlist, visited_resto, delete_wishlist,not_visited_resto, show_json, add_to_wishlist_flutter, visited_resto_flutter, delete_wishlist_flutter, get_json_resto_data
 
 app_name = 'wishlistresto'
 
@@ -13,4 +13,5 @@ urlpatterns = [
      path('add-to-wishlist-flutter/', add_to_wishlist_flutter, name='add_to_wishlist_flutter'),
      path('visited-flutter/<uuid:id>/', visited_resto_flutter, name='visited_resto_flutter'),
      path('deletewishlist-flutter/<uuid:id>/', delete_wishlist_flutter, name='delete_wishlist_flutter'),
+     path('restoJson/', get_json_resto_data, name='get_json_resto_data')
 ]
